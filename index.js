a5db3c3ebb075648b73105e5fb16a8f259e0a997
RidaRidss <ridarocks12@yahoo.com>
2015-02-17 19:25:15 +0500

    calling res.send and pass it a HTML string

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 /**
  * Created by junaid pc on 2/17/2015.
  */
+var app = require('express')();
+var http = require('http').Server(app);
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});

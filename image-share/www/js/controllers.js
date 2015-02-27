eaf303b0bc32f374c75cddd9ab7ec10587993979
RidaRidss <ridarocks12@yahoo.com>
2015-02-27 15:59:53 +0500

    commit

--- image-share/www/js/controllers.js
+++ image-share/www/js/controllers.js
@@ -1,3 +1,10 @@
 /**
  * Created by Junaid-Pc on 2/27/2015.
  */
+angular.module('com.htmlxprs.imageShare.controllers',[]).controller('HomeController',['$scope','USER','$state',function($scope,USER,$state){
+    $scope.user={};
+    $scope.next=function(){
+        USER.name=$scope.user.name;
+        $state.go('chat');
+    }
+}]);
